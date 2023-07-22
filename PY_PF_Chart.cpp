ac1277cb3931436f36a657478a64beec1b0c7091
David P. Riedel <davidp.riedel@gmail.com>
2023-07-21 22:05:20 -0400

    minimal working module for PF_Chart
    does nothing except verify it is empty

--- PY_PF_Chart.cpp
+++ PY_PF_Chart.cpp
@@ -17,8 +17,6 @@
 
 #include <decimal.hh>
 
-using decimal::Decimal;
-
 #include <pybind11/pybind11.h>
 
 namespace py = pybind11;
@@ -26,10 +24,6 @@ namespace py = pybind11;
 #include "PF_Chart.h"
 
 PYBIND11_MODULE(PY_PF_Chart, m) {
-    decimal::context_template = decimal::IEEEContext(decimal::DECIMAL64);
-    decimal::context_template.round(decimal::ROUND_HALF_UP);
-    decimal::context = decimal::context_template;
-
     py::class_<PF_Chart>(m, "PY_PF_Chart")
         .def(py::init())
         .def("empty", &PF_Chart::empty) ;
